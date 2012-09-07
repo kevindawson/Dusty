4f351499042c355db8934ee02a172c10897bcb82
bowtie <bowtie@cpan.org>
2012-09-07 16:39:53 +0100

    install depandencies for botbot

---
+++ cpan_bot.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use 5.014;
+use strict;
+use warnings;
+
+# Turn on $OUTPUT_AUTOFLUSH
+$| = 1;
+
+use feature 'unicode_strings';
+
+# use open qw( :encoding (UTF-8) :STD );
+
+use autodie;
+use diagnostics;
+# use Data::Printer {caller_info => 1, colored => 1,};
+
+say 'START';
+
+use CPAN;
+
+CPAN::HandleConfig->load;
+CPAN::Shell::setup_output;
+CPAN::Index->reload;
+
+
+my @modules
+  = qw( POE POE::Component::IRC::State YAML::XS Data::Printer DBD::SQLite DBI IRC::Utils DateTime );
+
+# install Padre on top of perlbrew and cpan_new.pl:
+for my $mod (@modules) {
+  CPAN::Shell->install($mod);
+}
+
+
+say 'END';
+
+1;
+
+__END__
