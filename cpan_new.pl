719a2a37ffbd1bacf4b0049db401cfe2b34409f9
bowtie <bowtie@cpan.org>
2012-12-21 12:10:29 +0000

    tweak for 5.12 support

--- cpan_new.pl
+++ cpan_new.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/env perl
 
-use 5.014;
+use 5.012;
 use strict;
 use warnings;
 
-# Turn on $OUTPUT_AUTOFLUSH
-$| = 1;
+use English qw( -no_match_vars ); # Avoids regex performance penalty
+$OUTPUT_AUTOFLUSH = 1;
 
-use feature 'unicode_strings';
+our $VERSION = '0.101';
+
 use autodie;
 use diagnostics;
 
-# use Data::Printer {caller_info => 1, colored => 1,};
 
 say 'START';
 
@@ -23,7 +23,7 @@ CPAN::Index->reload;
 
 
 my @modules =
-	qw( Term::ReadKey Term::ReadLine::Perl YAML YAML::XS CPAN::SQLite CPAN::Reporter JSON JSON::XS Test::Reporter::Transport::Metabase LWP::UserAgent Crypt::SSLeay LWP::Protocol::https IO::Socket::SSL Module::Install::DSL App::perlbrew App::cpanminus IPC::System::Simple );
+	qw( Term::ReadKey Term::ReadLine::Perl YAML YAML::XS CPAN::SQLite CPAN::Reporter JSON JSON::XS Test::Reporter::Transport::Metabase LWP::UserAgent Crypt::SSLeay LWP::Protocol::https IO::Socket::SSL Module::Install::DSL App::perlbrew App::cpanminus App::pmuninstall IPC::System::Simple );
 
 # install into a virgin perlbrew:
 for my $mod (@modules) {
