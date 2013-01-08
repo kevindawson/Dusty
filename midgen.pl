fde8c4f6f0b1416302f1af264ee7297dfa8aaf79
bowtie <bowtie@cpan.org>
2013-01-08 22:40:24 +0000

    add getops so you can use --help or -? now

--- midgen.pl
+++ midgen.pl
@@ -4,6 +4,7 @@ use v5.14;
 use strict;
 use warnings;
 
+
 our $VERSION = '0.03';
 use English qw( -no_match_vars ); # Avoids regex performance penalty
 local $OUTPUT_AUTOFLUSH = 1;
@@ -22,9 +23,8 @@ use PPI;
 use Module::CoreList;
 use CPAN;
 
-use Carp::Always::Color;
+# use Carp::Always::Color;
 
-say 'START';
 
 # check if there are versions in every module and if they are in the same
 # allow updating version numbers to one specific version.
@@ -34,19 +34,113 @@ use File::Slurp qw(read_file write_file);
 my @requires      = ();
 my %requires      = ();
 my %test_requires = ();
+my @package_names;
+my $package_name;
+my $format = 'mi'; # dsl | mi | build
 
-my $format      = 'mi'; # dsl | mi | build
-my $ignore_base = 1;    # 1 true ignore perl base functions
+# my $ignore_base = 1;    # 1 true ignore perl base functions
+
+my @posiable_directories_to_search = ();
+my @directories_to_search          = ();
 
 # my $mod_version = 'current';
 my $mod_version = 5.010000;
 
+use Getopt::Long;
+Getopt::Long::Configure("bundling");
+use Pod::Usage;
+my $help        = 0;
+my $base_parent = 0; # 1 true ignore perl base functions
+my $core        = 0; # show perl core modules as well
+my $verbose     = 0; # option variable with default value (false)
+my $mojo        = 0; # 1 true ignore Mojo detection
+my $debug       = 0; # lots of good stuff here :)
+GetOptions(
+	'verbose|v'       => \$verbose,
+	'core|c'          => \$core,
+	'base|parent|b|p' => \$base_parent,
+	'help|h|?'        => \$help,
+	'mojo|m'          => \$mojo,
+	'debug|d'         => sub { $core = 1; $verbose = 1, $base_parent = 0; $mojo =0; $debug = 1; },
+) or pod2usage(2);
+pod2usage(1) if $help;
+
+
+
+say 'START';
+
 try {
 	find( \&first_package_name, 'lib' );
 };
-my @package_names;
+p @package_names if $debug;
+$package_name = $package_names[0];
+say 'Package: ' . $package_names[0];
+
+
+
+if ( defined -d "./lib" ) {
+	if ( -d _ ) {
+
+		# print "somefile is a directory\n";
+	} else {
+
+		# print "somefile is not a directory\n";
+	}
+} else {
+	print "somefile doesn't exist\n";
+}
+
+# Find required modules
+@posiable_directories_to_search = qw( lib scripts bin );
+@directories_to_search          = ();                   # = qw( lib scripts bin );
+
+# p @posiable_directories_to_search;
+for my $directory (@posiable_directories_to_search) {
+
+	# p $directory;
+	if ( defined -d $directory ) {
+
+		# say 'ok';
+		push @directories_to_search, $directory;
+	}
+}
+
+# p @directories_to_search;
+try {
+	# find( \&requires, 'lib' );
+	find( \&requires, @directories_to_search );
+};
+
+
+
+output( 'requires', \%requires );
+print "\n";
+
+# remove_children( \%requires );
+
+
+# Find test_required modules
+@posiable_directories_to_search = qw( t );
+@directories_to_search          = ();
+for my $directory (@posiable_directories_to_search) {
+	if ( defined -d $directory ) {
+		push @directories_to_search, $directory;
+	}
+}
+
+try {
+	find( \&test_requires, @directories_to_search );
+};
+
+output( 'test_requires', \%test_requires );
+
+
+print "\n";
+say 'END';
+
 
 sub first_package_name {
+	my $self = shift;
 	return if $_ !~ /\.pm$/;
 
 	# say 'input: ' . $_;
@@ -54,45 +148,17 @@ sub first_package_name {
 
 	# Load a Document from a file
 	my $document = PPI::Document->new($_);
-	my $ppi_sp = $document->find('PPI::Statement::Package');
+	my $ppi_sp   = $document->find('PPI::Statement::Package');
 
 	push @package_names, $ppi_sp->[0]->namespace;
 }
-my $package_name = $package_names[0];
-say 'Package: ' . $package_names[0];
-
-
-
-    if (defined -d "./lib") {
-        if (-d _) {
-            print "somefile is a directory\n";
-        } else {
-            print "somefile is not a directory\n";
-        }
-    } else {
-        print "somefile doesn't exist\n";
-    }
-    
-my @posiable_directories_to_search = qw( lib scripts bin );
-my @directories_to_search = (); # = qw( lib scripts bin );
-# p @posiable_directories_to_search;
-for my $directory (@posiable_directories_to_search) {
-	# p $directory;
-	if ( defined -d $directory ) {
-		say 'ok';
-		push @directories_to_search, $directory;
-	}
-}
-p @directories_to_search;
-try {
-	# find( \&requires, 'lib' );
-	find( \&requires, @directories_to_search );
-};
 
 sub requires {
 	return if $_ !~ /\.p[lm]$/;
 
-	# say 'input: ' . $_;
+	if ($verbose) {
+		say 'looking for requires in: ' . $_;
+	}
 	my @items = ();
 
 	# Load a Document from a file
@@ -110,35 +176,21 @@ sub requires {
 
 				# my $module  = $include->module;
 				my @modules = $include->module;
-				if ($ignore_base) {
+				if ( !$base_parent ) {
 
-					# undef(@modules);
-					if ( $include->module =~ /(base|parent)/ ) {
-						say 'Info: check ' . $include->pragma . ' pragma: ';
-						say $include->content;
-
-						my $content = $include->content;
-						$content =~ s/^use (base|parent) //;
-
-						$content =~ s/^qw[\<|\(|\{|\[]\n?\t?\s*//;
-						$content =~ s/\s*[\>|\)|\}|\]];\n?\t?$//;
-						$content =~ s/(\n\t)/, /g;
-
-						# chomp $content;
-						@modules = ();
-						undef(@modules);
-						@modules = split /, /, $content;
-
-						# p @modules;
+					my @base_parent_modules = base_parent( $include->module, $include->content, $include->pragma );
+					if (@base_parent_modules) {
+						@modules = @base_parent_modules;
 					}
+
 				}
 
 				foreach my $module (@modules) {
 
 					# p $module;
-					if ($ignore_base) {
+					if ( !$core ) {
 
-						# p $module;
+						p $module if $debug;
 						next if Module::CoreList->first_release($module);
 					}
 
@@ -151,8 +203,8 @@ sub requires {
 						# don't include our own packages here
 						next;
 					}
-					if ( $module =~ /Mojo/ ) {
-					$module = 'Mojolicious';
+					if ( $module =~ /Mojo/ && !$mojo ) {
+						$module = 'Mojolicious';
 					}
 					if ( $module =~ /^Padre/ && $module !~ /^Padre::Plugin::/ ) {
 
@@ -176,11 +228,6 @@ sub requires {
 								# say $module.' cpan mod version = undef';
 								$requires{$module} = $mod->cpan_version;
 							}
-
-							# if ( $mod->cpan_version && $mod->cpan_version ne 'undef' ) {
-							# say $module.' cpan mod version = undef';
-							# $requires{$module} = $mod->cpan_version;
-							# }
 						}
 					};
 				}
@@ -191,29 +238,13 @@ sub requires {
 	push @requires, @items;
 }
 
-output( 'requires', \%requires );
-
-# remove_children( \%requires );
-
-@posiable_directories_to_search = qw( t );
-@directories_to_search          = (); 
-for my $directory (@posiable_directories_to_search) {
-	if ( defined -d $directory ) {
-		push @directories_to_search, $directory;
-	}
-}
-
-try {
-	find( \&test_requires, @directories_to_search );
-};
-
 sub test_requires {
 
 	return if $_ !~ /\.(t|pm)$/;
 
-	# return if $_ !~ /\.pm$/;
-
-	# p $_;
+	if ($verbose) {
+		say 'looking for test_requires in: ' . $_;
+	}
 	my @items = ();
 
 	# Load a Document from a file
@@ -243,26 +274,10 @@ sub test_requires {
 			if ( $include->pragma !~ /(strict|warnings)/ ) {
 
 				my @modules = $include->module;
-				if ($ignore_base) {
-
-					# undef(@modules);
-					if ( $include->module =~ /(base|parent)/ ) {
-						say 'Info: check ' . $include->pragma . ' pragma: ';
-						say $include->content;
-
-						my $content = $include->content;
-						$content =~ s/^use (base|parent) //;
-
-						$content =~ s/^qw[\<|\(|\{|\[]\n?\t?\s*//;
-						$content =~ s/\s*[\>|\)|\}|\]];\n?\t?$//;
-						$content =~ s/(\n\t)/, /g;
-
-						# chomp $content;
-						@modules = ();
-						undef(@modules);
-						@modules = split /, /, $content;
-
-						# p @modules;
+				if ( !$base_parent ) {
+					my @base_parent_modules = base_parent( $include->module, $include->content, $include->pragma );
+					if (@base_parent_modules) {
+						@modules = @base_parent_modules;
 					}
 
 				}
@@ -270,9 +285,9 @@ sub test_requires {
 				foreach my $module (@modules) {
 
 					# p $module;
-					if ($ignore_base) {
+					if ( !$core ) {
 
-						# p $module;
+						p $module if $debug;
 						next if Module::CoreList->first_release($module);
 					}
 
@@ -283,8 +298,8 @@ sub test_requires {
 						# don't include our own packages here
 						next;
 					}
-					if ( $module =~ /Mojo/ ) {
-					$module = 'Mojolicious';
+					if ( $module =~ /Mojo/ && !$mojo) {
+						$module = 'Mojolicious';
 					}
 					if ( $module =~ /^Padre/ && $module !~ /^Padre::Plugin::/ ) {
 
@@ -313,7 +328,7 @@ sub test_requires {
 							# p $module;
 							my $mod = CPAN::Shell->expand( "Module", $module );
 
-							# p $mod;
+							p $mod if $debug;
 
 							if ($mod) { # next if not defined $mod;
 								if ( $mod->cpan_version ) {
@@ -330,7 +345,33 @@ sub test_requires {
 	}
 }
 
-output( 'test_requires', \%test_requires );
+sub base_parent {
+	my $module  = shift;
+	my $content = shift;
+	my $pragma  = shift;
+	my @modules = ();
+	if ( $module =~ /(base|parent)/ ) {
+
+		if ($verbose) {
+			say 'Info: check ' . $pragma . ' pragma: ';
+			say $content;
+		}
+
+		$content =~ s/^use (base|parent) //;
+
+		$content =~ s/^qw[\<|\(|\{|\[]\n?\t?\s*//;
+		$content =~ s/\s*[\>|\)|\}|\]];\n?\t?$//;
+		$content =~ s/(\n\t)/, /g;
+
+		# my @modules = ();
+		@modules = split /, /, $content;
+
+		p @modules if $debug;
+	}
+	return @modules;
+}
+
+
 
 sub output {
 	my $title        = shift || 'title missing';
@@ -375,9 +416,66 @@ sub remove_children {
 }
 
 
-print "\n";
-say 'END';
+
 
 exit(0);
 
 __END__
+
+
+=head1 NAME
+ 
+sample - Using Getopt::Long and Pod::Usage
+ 
+=head1 SYNOPSIS
+ 
+sample [options]
+ 
+ Options:
+   -help	brief help message
+   -core	show perl core modules
+   -base	check base includes
+   -verbose	
+   -debug	lots of stuff
+   
+=head1 OPTIONS
+ 
+=over 4
+ 
+=item B<--help or -h>
+ 
+Print a brief help message and exits.
+ 
+=item B<-core or -c>
+
+Shows modules that are in Perl core
+
+=item B<--verbose or -v>
+
+Show file that are being checked
+
+also show contents of base|parent check
+
+=item B<--parent or -p>
+
+alternative  --base or -b
+
+Turn Off - try to include the contents of base|parent modules as well
+
+=item B<--mojo or -m>
+
+Turn Off - the /Mojo/ to Mojolicious catch
+
+=item B<--debug or -d>
+
+equivalent of -cv and some :)
+
+=back
+ 
+=head1 DESCRIPTION
+ 
+B<This program> will read the given input file(s) and do something
+useful with the contents thereof.
+ 
+=cut
+
