e3de7943d9871f7410e2c1a997f88becbd4c58c2
bowtie <bowtie@cpan.org>
2013-01-12 13:13:49 +0000

    drop to 5.010001 and a quick tweak and tidy

--- cpan_new.pl
+++ cpan_new.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-use 5.012;
+use 5.010001;
 use strict;
 use warnings;
 
@@ -12,24 +12,39 @@ our $VERSION = '0.101';
 use autodie;
 use diagnostics;
 
-
-say 'START';
-
 use CPAN;
 
 CPAN::HandleConfig->load;
 CPAN::Shell::setup_output;
 CPAN::Index->reload;
 
+say 'START';
 
-my @modules =
-	qw( Term::ReadKey Term::ReadLine::Perl YAML YAML::XS CPAN::SQLite CPAN::Reporter JSON JSON::XS Test::Reporter::Transport::Metabase LWP::UserAgent Crypt::SSLeay LWP::Protocol::https IO::Socket::SSL Module::Install::DSL App::perlbrew App::cpanminus App::pmuninstall IPC::System::Simple );
+my @modules = qw(
+	Term::ReadKey
+	Term::ReadLine::Perl
+	YAML
+	YAML::XS
+	CPAN::SQLite
+	CPAN::Reporter
+	JSON
+	JSON::XS
+	Test::Reporter::Transport::Metabase
+	Crypt::SSLeay
+	LWP::UserAgent
+	LWP::Protocol::https
+	IO::Socket::SSL
+	Module::Install::DSL
+	App::perlbrew
+	App::cpanminus
+	App::pmuninstall
+	IPC::System::Simple
+);
 
 # install into a virgin perlbrew:
 for my $mod (@modules) {
 	CPAN::Shell->install($mod);
 }
-
 
 say 'END';
 
